7d7b40502c843a913e6c6aaded1cf1efc0d036e5
Wixden <watchonlytutorial@gmail.com>
2023-11-12 01:16:04 +0600

    find insert filter in mongoDB

--- Module-5/find-insert-filter.js
+++ Module-5/find-insert-filter.js
@@ -1,1 +1,27 @@
+// =================================================================
+
 // MongoDB (find, findOne, insertMany, insertOne, )
+
+// MongoDB Find:
+// It will return all documents inside the test collection
+db.test.find();
+
+// This will return only one document inside the test collection
+db.test.findOne();
+
+// This will return only the specified document
+db.test.findOne({ gender: "Female" });
+
+// MongoDB Insert:
+
+// This will add/insert one document into the test collection (name, and gender)
+db.test.insertOne({ name: "John", gender: "Male" });
+
+// This will add/insert as many as we want document into the test collection
+// We will have to provide an array inside of insertMany()
+db.test.insertMany([
+  { name: "Hasan", gender: "Male" },
+  { name: "Sefali", gender: "Female" },
+]);
+
+// =================================================================
