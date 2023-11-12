374357a7a78d4702438957ad4154a9278e289af0
Wixden <watchonlytutorial@gmail.com>
2023-11-12 07:55:20 +0600

    MongoDB set, addToSet and push

--- Module-5/5.8.js
+++ Module-5/5.8.js
@@ -29,4 +29,42 @@ db.test2.updateOne(
   }
 ];
 
+// Now we can't always use $set to update a document. because it replace the existing document with the new one. And we lose the previous data. In this case when we want to update the document with new value and also don't want to lose the previous value, we need to use $addToSet. This will add the new value and it will not repeat if the value already exists.
+db.test2.updateOne(
+  { _id: ObjectId("654fd09741d297c5afb69a41") },
+  { $addToSet: { skills: { $each: ["PHP", "TypeScript"] } } }
+);
+
+// Results:
+[
+  {
+    acknowledged: true,
+    matchedCount: 1,
+    modifiedCount: 1,
+  },
+];
+
+// Results 2: // updating it second time with the same value will not modify the existing document.
+[
+  {
+    acknowledged: true,
+    matchedCount: 1,
+    modifiedCount: 0,
+  },
+];
+
+// What if we want to repeat the update event if the value already exist, In this case we will sue the $push operator. This will keep updating/adding the new value to the specified field as much as we want.
+db.test2.updateOne(
+  { _id: ObjectId("654fd09741d297c5afb69a41") },
+  { $push: { skills: { $each: ["PHP", "TypeScript"] } } }
+);
+
+// Results:
+[
+  {
+    acknowledged: true,
+    matchedCount: 1,
+    modifiedCount: 1,
+  },
+];
 // =================================================================
